906b821c1cdafde17648e13a431ed93dc7cb3399
Tomonari.sato <muzak1001@outlook.jp>
2019-02-19 13:31:22 +0000

    newname_form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,23 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def project_params
+    params.require(:project).permit(:first_name, :last_name, :age)
+  end  
 end
